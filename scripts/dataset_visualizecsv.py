897097da4b2c89c3246dce4604fb1f771b787a88
srobledo2021 <113594786+srobledo2021@users.noreply.github.com>
2025-06-29 19:45:34 +0200

    Update dataset_visualizecsv.py

--- scripts/dataset_visualizecsv.py
+++ scripts/dataset_visualizecsv.py
@@ -9,11 +9,14 @@ CSV_PATH = "dataset/dataset_Deepracer_BaseMap_1751132511826.csv"
 df = pd.read_csv(CSV_PATH)
 
 pygame.init()
-screen = pygame.display.set_mode((1600, 700))
+screen = pygame.display.set_mode((1750, 1000))
 pygame.display.set_caption("Visualizador Dataset DeepRacer (solo gráficas)")
 
 font = pygame.font.SysFont(None, 26)
 clock = pygame.time.Clock()
+
+ruta_imagen = "/home/sergior/Downloads/pruebas/dataset/masks/mask1751132511826/1751125319931_mask_Deepracer_BaseMap_1751132511826.png"
+imagen = pygame.image.load(ruta_imagen).convert_alpha()
 
 
 def render_plot(data_slice, current_idx, window_duration=500):
@@ -72,7 +75,7 @@ def render_plot(data_slice, current_idx, window_duration=500):
 
 index = 0
 running = True
-
+mid_path_name = "/home/sergior/Downloads/pruebas/dataset/"
 while running and index < len(df):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -82,14 +85,25 @@ while running and index < len(df):
     plot_surface = render_plot(df, index)
 
     screen.fill((20, 20, 20))
-    screen.blit(plot_surface, (600, 100))
+    screen.blit(plot_surface, (800, 100)) 
 
+ 
     txt = f"Frame: {index} | Timestamp: {int(row['timestamp'])}"
     text_surf = font.render(txt, True, (255, 255, 255))
     screen.blit(text_surf, (50, 10))
+
+    imagen_rgb_path = mid_path_name + row.iloc[0]
+    img_rgb = pygame.image.load(imagen_rgb_path).convert_alpha() 
+    screen.blit(img_rgb, (0, 40))  
+
+    imagen_mask_path = mid_path_name + row.iloc[1]
+    img_mask = pygame.image.load(imagen_mask_path).convert_alpha()
+    screen.blit(img_mask, (0, 500))
 
     pygame.display.flip()
     time.sleep(0.033)
     index += 1
 
 pygame.quit()
+
+
