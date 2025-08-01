e890b16baaf040d12e0a29d4ba67fc5170080372
srobledo2021 <113594786+srobledo2021@users.noreply.github.com>
2025-08-01 14:57:47 +0200

    Update receiver.py for ps4

--- scripts/clientCarlaScripts/receiver.py
+++ scripts/clientCarlaScripts/receiver.py
@@ -6,12 +6,13 @@ PORT = 1977
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    print(f"🟢 Esperando conexión en {HOST}:{PORT}...")
+    print(f"Waiting for connection on {HOST}:{PORT}...")
     conn, addr = s.accept()
     with conn:
-        print(f"🔌 Conectado desde {addr}")
+        print(f"Connected from {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            print(f"🎮 Datos recibidos: {data.decode().strip()}")
+            msg = data.decode().strip()
+            print(f"{msg}")
